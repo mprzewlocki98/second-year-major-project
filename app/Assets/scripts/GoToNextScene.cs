74f126022a50f5d2cff039248bccbda111580e46
weiyi-peng <weiyi.peng@kcl.ac.uk>
2018-02-15 14:03:28 +0000

    select movie functionality

---
+++ app/Assets/scripts/GoToNextScene.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class GoToNextScene : MonoBehaviour {
+
+    public void ChangeScene(string sceneName){
+        Application.LoadLevel(sceneName);
+    }
+}
