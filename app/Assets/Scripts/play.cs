52e93d4377aa64a7b74d27c6d182f0d5cca486fb
EmanuelaNedelcu <emanuela.nedelcu@kcl.ac.uk>
2018-02-15 11:25:41 +0000

    added scoring functionality

--- app/Assets/Scripts/play.cs
+++ app/Assets/Scripts/play.cs
@@ -4,6 +4,13 @@ using UnityEngine.SceneManagement;
 public class play : MonoBehaviour {
 
     public string sceneName;
+    // total score keeps track of the overall score of the player
+    private int totalScore = 0;
+    // minigame score is the current minigame's score; when completed is added to the total score then is reset to 0
+    private int minigameScore = 0;
+    // tracer minigame specific variables
+    //TODO @ Emma: move these within a separate area with restricted access
+    private int tracerSpotsTapped = 0, tracerSpotsTotal = 4;
 
     /* Adding functionality to a button (or another component):
      *    
@@ -35,14 +42,33 @@ public class play : MonoBehaviour {
             Vector2 pos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
             RaycastHit2D hitInfo = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(pos), Vector2.zero);
            
+            //TODO @ Emma: increase tag area for younger age category
             if (hitInfo && hitInfo.transform.gameObject.tag == "spot")
             {
                 Debug.Log(hitInfo.transform.gameObject.name);
                 Debug.Log("Spot tapped!");
+                minigameScore ++;
+                tracerSpotsTapped++;
+                if (tracerSpotsTapped == 4)
+                {
+                    Debug.Log("Tracer Minigame score: " + minigameScore);
+                    // add minigame score to total player's score
+                    if (minigameScore > 0)
+                    {
+                        totalScore += minigameScore;
+                    }
+                    // reset minigame score for next minigame
+                    minigameScore = 0;
+                    // load next scene
+                    LoadScene("gameplay3 dog");
+                }
             }
             else
             {
+                // tapping outside the target spots areas lossed the player a point
+                //TODO @ Emma: only older age category should lose points
                 Debug.Log("Missed!");
+                minigameScore--;
             }
         }
     }
