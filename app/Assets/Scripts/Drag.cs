4f6a7226ac05bc5a28eec7aaaadba9c09a1a3c12
YurunSONG <yurun.song@kcl.ac.uk>
2018-02-16 18:46:11 +0000

    initialize

---
+++ app/Assets/Scripts/Drag.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drag : MonoBehaviour {
+	float decaySecond = 1f;
+	float elaspedSecond = 0;
+	float distance = 10;
+	bool flag = false;
+	GameObject obj;
+
+	void OnMouseDrag(){
+		obj = GetComponent<GameObject> ();
+		Vector3 mousePosition = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, distance);
+		Vector3 objPosition = Camera.main.ScreenToWorldPoint (mousePosition);
+		transform.position = objPosition;
+	}
+
+	void OnMouseUp () {
+		flag = true;
+		Destroy(this.gameObject,decaySecond); //delete
+//		gameObject.active = false; //hide
+//		renderer.enabled = false;
+	}
+
+	void Update(){
+		if (flag) {
+			float scaleRate = (decaySecond - elaspedSecond)/decaySecond;
+			transform.localScale *= scaleRate;
+			elaspedSecond += Time.deltaTime;
+		}
+			
+	}
+}
--- app/Assets/Scripts/play.cs
+++ app/Assets/Scripts/play.cs
@@ -1,24 +1,70 @@
-﻿using UnityEngine;
-using UnityEngine.SceneManagement;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class play : MonoBehaviour {
+	float hintSecond = 1f;
+	float speed = 10f; //how fast it shakes
+	float amount = 2f;//how much it shakes
+	bool processEnd = true;
+	Vector3 mousePos;
 
-    public string sceneName;
+	// Use this for initialization
+	void Start () {
+		hintSecond = Time.time;
+		mousePos = new Vector3 (0,0,0);
+	}
+		
+	// Update is called once per frame
+	void Update () {
+		GameObject obj = GameObject.FindWithTag ("item");
 
-    /* Adding functionality to a button (or another component):
-     *    
-     *    attach the c# script to the button:
-     *    [if it's a new script, first create it in project view/scripts
-     *     then code; make sure the function is of public void type with 0/1 arguments]
-     *    drag the script from project view to the desired button in hierarchy view.
-     *    
-     *    in inspector view in the on_click() functionality:
-     *    select an object (new window pops, find the button in /scene/ option).
-     *    select the script/method and optionally write an argument then ctrl_s
-     */
-    public void LoadScene(string sceneName)
-    {
-        SceneManager.LoadScene(sceneName);
-    }
+		if (obj != null && Time.time - hintSecond >= 5) {
+			
+			SpriteRenderer SR = obj.GetComponent<SpriteRenderer> ();
 
+			if (amount > 0) {
+				processEnd = false;
+				SR.color = new Color (SR.color.r, SR.color.g, SR.color.b, Mathf.Sin (Time.time * speed));
+				amount -= Time.deltaTime;
+
+			} else {
+
+				SR.color = new Color (SR.color.r, SR.color.g, SR.color.b, 1);
+				amount = 2f;
+				hintSecond = Time.time;
+				processEnd = true;
+			}	
+		}
+			
+		if (Input.mousePosition != mousePos && processEnd) {
+			amount = 2f;
+			hintSecond = Time.time;
+//			Debug.Log ("mouse click");
+			mousePos = Input.mousePosition;
+		}
+	}
 }
+
+
+
+//				obj.transform.localScale = new Vector3 (scale);
+//
+//				if (obj.transform.localScale.x < 0.9f) {
+//					obj.transform.localScale.Set(0.9f,0.9f,0);
+//					scale = 1.1f;
+//					scale += Time.deltaTime * 0.1f;
+//				} else if (obj.transform.localScale.x > 1.1f) {
+//					obj.transform.localScale.Set(1.1f,1.1f,0);
+//					scale = 0.9f;
+//					scale += Time.deltaTime * 0.1f;
+//				} else {
+//					scale += Time.deltaTime * 0.1f;
+//				}
+
+
+//				obj.transform.Rotate (Mathf.Sin (Time.time * speed),Mathf.Sin (Time.time * speed), 0);
+//				Vector3 v1 = new Vector3 (25, 25, 0);
+//				obj.transform.Rotate(v1*Time.deltaTime);
+//				Vector3 v2 = new Vector3 (25, -25, 0);
+//				obj.transform.Rotate(v2*Time.deltaTime);
