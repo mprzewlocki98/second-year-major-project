c166accba61b6580c6d700bec098ecbcab94c270
k1630989 <ahmad.bin_mohamad_pauzi@kcl.ac.uk>
2018-02-18 23:34:09 +0000

    Creating the first game: Following the Green Lights

---
+++ app/Assets/Scripts/LightScript.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class LightScript : MonoBehaviour {
+
+	public List<Sprite> lightImages;
+
+	private GameControllerScript gcScript;
+
+	public bool isActive = false;
+	public bool isGreen = false;
+
+	// Use this for initialization
+	void Start () {
+		ResetPosition();
+		gcScript = this.transform.parent.GetComponent<GameControllerScript>();
+	}
+
+	public void MakeDeadly() {
+		isGreen = true;
+	}
+
+	public void MakeFriendly() {
+		isGreen = false;
+		int lightChoice = Random.Range (0, lightImages.Count);
+		this.GetComponent<SpriteRenderer>().sprite = lightImages[lightChoice];
+	}
+
+	void ResetPosition() {
+		this.transform.position = new Vector3(0.0f, -10.0f, 0.0f);
+	}
+
+	void Update() {
+		if (this.transform.position.y > 6.0f && isActive) {
+			Deactivate();
+		}
+	}
+
+	public void Activate() {
+		isActive = true;
+		float upSpeed = Random.Range (1.5f, 4.0f);
+		this.GetComponent<Rigidbody2D>().velocity = new Vector3(0.0f, upSpeed, 0.0f);
+		this.transform.position = new Vector3(Random.Range (-2.4f, 2.45f), -6.0f, 0.0f);
+		if (Random.Range(0,4) == 0) {
+			MakeDeadly();
+		} else {
+			MakeFriendly ();
+		}
+	}
+
+	public void Deactivate() {
+		isActive = false;
+		this.GetComponent<Rigidbody2D>().velocity = Vector3.zero;
+		ResetPosition();
+	}
+
+	void OnMouseDown() {
+		Pop ();
+	}
+
+	public void Pop() {
+		if (isGreen) {
+			Deactivate();
+		} else {
+			gcScript.AddPoints(1);
+			Deactivate ();
+		}
+	}
+}
