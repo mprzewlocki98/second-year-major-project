753519070ee7a8f03bc76fd620eaf7286cce397e
k1630989 <ahmad.bin_mohamad_pauzi@kcl.ac.uk>
2018-02-19 02:38:07 +0000

    Integrating first game to the main menu

--- app/Assets/Scripts/MainMenu.cs
+++ app/Assets/Scripts/MainMenu.cs
@@ -5,8 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
 
-	public void PlayGame() {
+	public void PlayGame(int page) {
 
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + page);
     }
 }
