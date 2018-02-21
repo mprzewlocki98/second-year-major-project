7d99cb1014e81c5a662ce4bcbb7c9a369da4bb35
Mateusz Przewlocki <mateusz.przewlocki@kcl.ac.uk>
2018-02-21 20:38:16 +0000

    Added basic template for unit testing
    Unity has built in unit testing. Yay!

---
+++ app/Assets/Scripts/Injection/Editor/InjectionSceneTest.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class InjectionSceneTest {
+
+	[Test]
+	public void InjectionSceneTestSimplePasses() {
+        Assert.AreEqual(1, 1);
+	}
+
+	[UnityTest]
+	public IEnumerator InjectionSceneTestWithEnumeratorPasses() {
+        Assert.AreEqual(1, 1);
+		yield return null;
+	}
+}
