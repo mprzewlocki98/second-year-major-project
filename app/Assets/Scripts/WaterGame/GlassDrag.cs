2542d421b30ebde286c4a31287b26fdeec0a1fc2
Adrian <adrian.kucharski@kcl.ac.uk>
2018-02-23 19:12:26 +0000

    Drag Functionality added to water glass

---
+++ app/Assets/Scripts/WaterGame/GlassDrag.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlassDrag : MonoBehaviour {
+
+    public bool canDrag = true;
+
+    private float distance = 10;
+    private GameObject obj = null;
+
+    void OnMouseDrag() { 
+        
+        if (canDrag) { 
+            obj = GetComponent<GameObject>();
+            Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance);
+            Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+            transform.position = objPosition;
+        }
+    }
+
+    void OnMouseUp()
+    {
+        
+    }
+}
