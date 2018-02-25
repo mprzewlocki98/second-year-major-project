924d845edb7b19a584792c9213f4bfd4d5ab0c41
Adrian <adrian.kucharski@kcl.ac.uk>
2018-02-25 17:05:28 +0000

    Added Water Fill on button Click

---
+++ app/Assets/Scripts/WaterGame/ButtonClick.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonClick : MonoBehaviour {
+
+    public GameObject[] waterArray;
+    private int nextWaterComponent = 0;
+
+	void Start () {
+		
+        for (int i = 0; i < waterArray.Length; i++) {
+
+            waterArray[i].SetActive(false);
+        }
+	}
+	
+    void OnMouseDown(){
+
+        if (nextWaterComponent < waterArray.Length) {
+
+            waterArray[nextWaterComponent].SetActive(true);
+            nextWaterComponent++;
+        }
+    }
+	
+	void Update () {
+		
+	}
+}
