15e8595b9b430c0f2973c64d8028a69b055b9978
k1630989 <ahmad.bin_mohamad_pauzi@kcl.ac.uk>
2018-02-26 03:10:37 +0000

    cleaning up the light game completion script

--- app/Assets/Scripts/GreenLightsGame/CompleteLightGame.cs
+++ app/Assets/Scripts/GreenLightsGame/CompleteLightGame.cs
@@ -11,38 +11,39 @@ public class CompleteLightGame : MonoBehaviour {
 	bool showIcon = true;
 
 	void Start(){
+        // linking the variables to the components
 		anim = GetComponent<Animation> ();
         gc = GetComponent<GameControllerScript>();
 	}
 
 	void Update(){
+        // this will always check if player has won the game
         successDelegate methodToUse = checkSuccess;
         played = methodToUse.Invoke();
 
-        if (played)
-        {
-            if (showIcon)
-            {
+        if (played) {
+            if (showIcon) {
+                // makes the sprite renderer visible
                 gc.setSprender(true);
+                // play the animation
                 anim.Play("wellDone");
+                // stop the animation after once played
                 showIcon = false;
             }
+
+            // Invoke allows a certain waiting time
             Invoke("PlayerWon", 1);
         }
     }
 
-    void PlayerWon()
-    {
+    void PlayerWon() {
         SceneManager.LoadScene("4-cutscene");
     }
 
-
     delegate bool successDelegate();
 
-    bool checkSuccess()
-    {
-        if (gc.getPlayerScore() == 1)
-        {
+    bool checkSuccess() {
+        if (gc.getPlayerScore() == 10) {
             return true;
         }
         return false;
