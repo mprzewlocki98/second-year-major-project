47cbdd32c040c3b83d88a2047ca74cbbf48ffedf
k1630989 <ahmad.bin_mohamad_pauzi@kcl.ac.uk>
2018-02-26 00:38:03 +0000

    cleaning up code

--- app/Assets/Scripts/GreenLightsGame/GameControllerScript.cs
+++ app/Assets/Scripts/GreenLightsGame/GameControllerScript.cs
@@ -12,16 +12,21 @@ public class GameControllerScript : MonoBehaviour {
     private int playerScore = 0;
 	private float timeSinceLastSpawn = 0.0f;
 	private float timeToSpawn = 0.0f;
-	private List<Transform> lights;
-    private const int LIGHTS_POOL = 35;
+
+	private List<Transform> lights; // refers to the list of lights to be spawned
+    private const int LIGHTS_POOL = 35; // the number of light objects
 
 	void Start () {
+        // this is during initialisation; the moment the game starts
 		lights = new List<Transform>();
+
+        // generate a light object and populate list
 		for (int i = 0; i < LIGHTS_POOL; i++) {
 			Transform oneLight = Instantiate(lightPrefab) as Transform;
 			oneLight.parent = this.transform;
 			lights.Add(oneLight);
 		}
+
 		SpawnLights();
 		GameStart();
 	}
@@ -30,9 +35,11 @@ public class GameControllerScript : MonoBehaviour {
     void Update () {
 		timeSinceLastSpawn += Time.deltaTime;
 		if (timeSinceLastSpawn >= timeToSpawn) {
+            // lights will spawn if the time is ready to spawn
 			SpawnLights();
 		}
         if (playerScore==10) {
+            // when the player has successfully won the game
             PlayerWon("4-cutscene");
         }
 	}
@@ -42,23 +49,24 @@ public class GameControllerScript : MonoBehaviour {
     }
 
 	void SpawnLights() {
-		timeSinceLastSpawn = 0.0f;
-		timeToSpawn = Random.Range (0.0f, 2.0f);
-		foreach (Transform b in lights) {
-            
-			LightScript ls = b.GetComponent<LightScript>();
+		timeSinceLastSpawn = 0.0f; // float is used for precision
+		timeToSpawn = Random.Range (0.0f, 2.0f); // random time generated to spawn light
+
+        foreach (Transform b in lights) {
+			LightScript ls = b.GetComponent<LightScript>(); // getComponent is to retrieve the script for lights
 			if (ls && !ls.isActive) {
+                // if ls is true and is not active, we will activate the light object
 				ls.Activate();
 				break;
 			}
 		}
 	}
 
+    // method that will add points when player has tapped a green light
 	public void AddPoints(int points=1) {
         playerScore += points;
         UpdateScoreDisplay();
 	}
-
 
 	void UpdateScoreDisplay() {
         scoreDisplay.text = "Score:  " + playerScore.ToString();
@@ -67,6 +75,5 @@ public class GameControllerScript : MonoBehaviour {
 	public void GameStart() {
 		UpdateScoreDisplay();
 	}
-
 
 }
