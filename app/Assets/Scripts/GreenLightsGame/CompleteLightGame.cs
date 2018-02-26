3fc62fb45c14ab8bb96be0b729ea7aacec7baba4
k1630989 <ahmad.bin_mohamad_pauzi@kcl.ac.uk>
2018-02-26 03:06:30 +0000

    adding a feature -well done- after game ends

---
+++ app/Assets/Scripts/GreenLightsGame/CompleteLightGame.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class CompleteLightGame : MonoBehaviour {
+
+	Animation anim;
+    GameControllerScript gc;
+	bool played = false;
+	bool showIcon = true;
+
+	void Start(){
+		anim = GetComponent<Animation> ();
+        gc = GetComponent<GameControllerScript>();
+	}
+
+	void Update(){
+        successDelegate methodToUse = checkSuccess;
+        played = methodToUse.Invoke();
+
+        if (played)
+        {
+            if (showIcon)
+            {
+                gc.setSprender(true);
+                anim.Play("wellDone");
+                showIcon = false;
+            }
+            Invoke("PlayerWon", 1);
+        }
+    }
+
+    void PlayerWon()
+    {
+        SceneManager.LoadScene("4-cutscene");
+    }
+
+
+    delegate bool successDelegate();
+
+    bool checkSuccess()
+    {
+        if (gc.getPlayerScore() == 1)
+        {
+            return true;
+        }
+        return false;
+    }
+}
--- app/Assets/Scripts/GreenLightsGame/GameControllerScript.cs
+++ app/Assets/Scripts/GreenLightsGame/GameControllerScript.cs
@@ -8,13 +8,11 @@ public class GameControllerScript : MonoBehaviour {
 
 	public Transform lightPrefab;
 	public Text scoreDisplay;
-    Animation anim;
+    SpriteRenderer sprender;
 
     private int playerScore = 0;
 	private float timeSinceLastSpawn = 0.0f;
 	private float timeToSpawn = 0.0f;
-    private bool played = false;
-    private bool showIcon = true;
 
     private List<Transform> lights; // refers to the list of lights to be spawned
     private const int LIGHTS_POOL = 35; // the number of light objects
@@ -24,7 +22,8 @@ public class GameControllerScript : MonoBehaviour {
         // this is during initialisation; the moment the game starts
 		lights = new List<Transform>();
 
-        anim = GetComponent<Animation>();
+        sprender = gameObject.GetComponent<SpriteRenderer>();
+        sprender.enabled = false;
 
         // generate a light object and populate list
         for (int i = 0; i < LIGHTS_POOL; i++) {
@@ -44,23 +43,6 @@ public class GameControllerScript : MonoBehaviour {
             // lights will spawn if the time is ready to spawn
 			SpawnLights();
 		}
-
-        successDelegate methodToUse = checkSuccess;
-        played = methodToUse.Invoke();
-
-        if (played) {
-            if (showIcon) {
-                anim.Play();
-                showIcon = false;
-            }
-            Invoke("PlayerWon", 1);
-        }
-    }
-
-    delegate bool successDelegate();
-
-    void PlayerWon() {
-        SceneManager.LoadScene("4-cutscene");
     }
 
 	void SpawnLights() {
@@ -77,10 +59,12 @@ public class GameControllerScript : MonoBehaviour {
 		}
 	}
 
-    bool checkSuccess() {
-        if (playerScore == 1){
-            return true;
-        } return false;
+    public void setSprender(bool x) {
+        sprender.enabled = x;
+    }
+
+    public int getPlayerScore() {
+        return playerScore;
     }
 
     // method that will add points when player has tapped a green light
