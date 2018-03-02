8da86f76cadc77381c11e67b8da69a545f9a6ae3
Mateusz Przewlocki <mateusz.przewlocki@kcl.ac.uk>
2018-03-02 12:02:50 +0000

    Add a test for the movie game

--- app/Assets/Scripts/Test/Editor/FilmsGameEditModeTest.cs
+++ app/Assets/Scripts/Test/Editor/FilmsGameEditModeTest.cs
@@ -15,4 +15,36 @@ public class FilmsGameEditModeTest {
         Assert.Pass();
     }
 
+    [Test]
+    public void AllComponentsPresent()
+    { // a template for tests in edit mode
+        EditorSceneManager.OpenScene("Assets/Scenes/11-films-game.unity", OpenSceneMode.Single);
+
+        GameObject b1, b2, b3, b4, b5, b6, b7, b8, script, panel, scrollPanel;
+
+        b1 = GameObject.Find("Button");
+        b2 = GameObject.Find("Button (1)");
+        b3 = GameObject.Find("Button (2)");
+        b4 = GameObject.Find("Button (3)");
+        b5 = GameObject.Find("Button (4)");
+        b6 = GameObject.Find("Button (5)");
+        b7 = GameObject.Find("Button (6)");
+        b8 = GameObject.Find("Button (7)");
+        script = GameObject.Find("Script");
+        panel = GameObject.Find("Panel");
+        scrollPanel = GameObject.Find("scrollPanel");
+
+        Assert.IsNotNull(b1);
+        Assert.IsNotNull(b2);
+        Assert.IsNotNull(b3);
+        Assert.IsNotNull(b4);
+        Assert.IsNotNull(b5);
+        Assert.IsNotNull(b6);
+        Assert.IsNotNull(b7);
+        Assert.IsNotNull(b8);
+        Assert.IsNotNull(script);
+        Assert.IsNotNull(panel);
+        Assert.IsNotNull(scrollPanel);
+    }
+
 }
