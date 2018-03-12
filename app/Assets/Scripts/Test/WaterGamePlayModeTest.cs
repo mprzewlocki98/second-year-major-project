d3d22de90cb1dd49b429d8d6305313cb6172dcc4
Adrian <adrian.kucharski@kcl.ac.uk>
2018-03-12 16:36:21 +0000

    Water game play mode test

--- app/Assets/Scripts/Test/WaterGamePlayModeTest.cs
+++ app/Assets/Scripts/Test/WaterGamePlayModeTest.cs
@@ -20,26 +20,6 @@ public class WaterGamePlayModeTest {
         yield return null;
     }
 
-    /*[UnityTest] // Test for player to go next scene once game has finished
-    public IEnumerator AllWaterSpritesAreNotActiveAtTheBeginningOfTheGame() {
-
-        GameObject water1 = GameObject.Find("water1");
-        GameObject water2 = GameObject.Find("water2");
-        GameObject water3 = GameObject.Find("water3");
-        GameObject water4 = GameObject.Find("water4");
-        GameObject water5 = GameObject.Find("water5");
-        GameObject water6 = GameObject.Find("water6");
-
-        Assert.IsFalse(water1.activeInHierarchy);
-        Assert.IsFalse(water2.activeInHierarchy);
-        Assert.IsFalse(water3.activeInHierarchy);
-        Assert.IsFalse(water4.activeInHierarchy);
-        Assert.IsFalse(water5.activeInHierarchy);
-        Assert.IsFalse(water6.activeInHierarchy);
-
-        yield return null;
-    }*/
-
     [UnityTest]  //Checks to see whether the glass is draggable at the beginning of the game
     public IEnumerator GlassIsDraggableAtStartOfGame() {
 
