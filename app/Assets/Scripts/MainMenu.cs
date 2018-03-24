b0e1cc8ce69a7b7bc7d03050849e6d2820b023cc
Adrian K <adrian.kucharski@kcl.ac.uk>
2018-03-24 21:48:17 +0000

    Added difficulty choice screen in main menu

--- app/Assets/Scripts/MainMenu.cs
+++ app/Assets/Scripts/MainMenu.cs
@@ -5,7 +5,20 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
 
-	public void PlayGame(int page) {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + page);
+    public GameObject choiceCanvas;
+
+	public void NextScene(int page) {
+
+        SceneManager.LoadScene(page);
+    }
+
+    public void SetActiveChoiceCanvas() {
+
+        choiceCanvas.SetActive(true);
+    }
+
+    public void SetNotActiveChoiceCanvas()
+    {
+        choiceCanvas.SetActive(false);
     }
 }
